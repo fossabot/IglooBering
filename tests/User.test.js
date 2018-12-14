4d80ab71d75ec570291c0efb3bc74fcd169889f1
Zanin Andrea <andrea@igloo.ooo>
2018-12-14 23:54:28 +0100

    baseline user tests

---
+++ tests/User.test.js
@@ -0,0 +1,105 @@
+import UserResolverFactory from "../graphql/resolvers/User";
+import MocksGenerator from "./mockUtils";
+import {
+  testScalarProp,
+  unauthenticatedShouldFail,
+  wrongIdShouldFail,
+  notAuthorizedShouldFail
+} from "./testUtils";
+
+const {
+  MockedBoard,
+  MockedUser,
+  mockBoardData,
+  mockUserData,
+  mockDeviceData,
+  MockedDevice,
+  MockedNotification,
+  mockNotificationData
+} = MocksGenerator();
+
+const UserResolver = UserResolverFactory({
+  User: MockedUser,
+  Board: MockedBoard,
+  Device: MockedDevice,
+  Notification: MockedNotification
+});
+
+describe("User", () => {
+  const testUserScalarProp = testScalarProp(UserResolver, { id: "mockUserId" }, mockUserData[0]);
+  const testUnauthenticated = unauthenticatedShouldFail(UserResolver, {
+    id: "mockUserId"
+  });
+  const testNotAuthorized = notAuthorizedShouldFail(
+    UserResolver,
+    { id: "mockUserId" },
+    { auth: { userId: "mockUserId4", tokenType: "TEMPORARY" } }
+  );
+
+  let privateScalarProps = [
+    "quietMode",
+    "devMode",
+    "monthUsage",
+    "emailIsVerified",
+    "paymentPlan",
+    "usageCap"
+  ];
+  let publicScalarProps = ["email", "name", "profileIcon", "profileIconColor"];
+
+  const scalarProps = [...privateScalarProps, ...publicScalarProps];
+  for (let prop of scalarProps) {
+    test(`${prop} is resolved correctly`, testUserScalarProp(prop));
+  }
+
+  test("settings is resolved correctly", async done => {
+    const settingsFound = await new Promise((resolve, reject) => {
+      UserResolver.settings(
+        { id: "mockUserId" },
+        {},
+        {
+          auth: { userId: "mockUserId", tokenType: "TEMPORARY" },
+          billingUpdater: { update: () => {} }
+        }
+      )(resolve, reject);
+    });
+
+    expect(settingsFound).toMatchObject({
+      language: mockUserData[0].settings_language,
+      timeFormat: mockUserData[0].settings_timeFormat,
+      timeZone: mockUserData[0].settings_timeZone,
+      dateFormat: mockUserData[0].settings_dateFormat,
+      temperature: mockUserData[0].settings_temperature,
+      lengthAndMass: mockUserData[0].settings_lengthAndMass
+    });
+
+    done();
+  });
+
+  const authorizedProps = [
+    ...privateScalarProps,
+    "settings",
+    "devices",
+    "deviceCount",
+    "pendingBoardShares",
+    "pendingBoardShareCount",
+    "pendingOwnerChanges",
+    "pendingOwnerChangeCount",
+    "boardCount",
+    "boards",
+    "notificationCount",
+    "notifications",
+    "values",
+    "valueCount",
+    "permanentTokens",
+    "permanentTokenCount"
+  ];
+  for (let prop of authorizedProps) {
+    test(`${prop} fails if not authorized`, testNotAuthorized(prop));
+  }
+
+  let authenticatedProps = [...authorizedProps, ...publicScalarProps];
+
+  for (let prop of authenticatedProps) {
+    test(`${prop} fails if unauthenticated`, testUnauthenticated(prop));
+  }
+});
