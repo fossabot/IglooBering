f5bdf34b9e779537e0faa5f7c8fad012982a1213
Zanin Andrea <andrea@igloo.ooo>
2019-02-08 12:32:12 +0100

    Change email only when the new one is verified

--- graphql/resolvers/Mutation.js
+++ graphql/resolvers/Mutation.js
@@ -1871,10 +1871,6 @@ const MutationResolver = (
       return authenticated(
         context,
         async (resolve, reject) => {
-          const userFound = await context.dataLoaders.userLoaderById.load(
-            context.auth.userId
-          )
-
           const sameEmailUserFound = await User.find({
             where: { email: args.newEmail },
           })
@@ -1884,18 +1880,8 @@ const MutationResolver = (
           }
 
           try {
-            const newUser = await userFound.update({
-              email: args.newEmail,
-              emailIsVerified: false,
-            })
+            sendVerificationEmail(args.newEmail, context.auth.userId)
             resolve(true)
-
-            pubsub.publish("userUpdated", {
-              userUpdated: newUser.dataValues,
-              userId: context.auth.userId,
-            })
-
-            sendVerificationEmail(args.newEmail, newUser.id)
 
             context.billingUpdater.update(MUTATION_COST)
           } catch (e) {
