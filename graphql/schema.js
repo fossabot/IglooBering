9622fe9670bb174c0fae3d7dd4a530970b6990d0
Zanin Andrea <andrea@igloo.ooo>
2019-02-28 23:15:43 +0100

    set up graphql custom directives

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,7 +1,29 @@
 import { makeExecutableSchema } from "graphql-tools"
 import fs from "fs"
+import { DirectiveLocation } from "graphql"
+import {
+  GraphQLCustomDirective,
+  applySchemaCustomDirectives,
+} from "graphql-custom-directive"
 import resolvers from "./resolvers"
 
 const typeDefs = fs.readFileSync("./graphql/types.graphql").toString()
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-export default makeExecutableSchema({ typeDefs, resolvers })
+const ToUpperDirective = new GraphQLCustomDirective({
+  name: "toUpperCase",
+  description: "change the case of a string to uppercase",
+  locations: [DirectiveLocation.FIELD],
+  resolve(resolve, root) {
+    return resolve().then(result => {
+      if (!result.toUpperCase)
+        throw new Error("toUpperCase directive is only available on strings")
+      return result.toUpperCase ? result.toUpperCase() : result
+    })
+  },
+})
+
+schema._directives.push.apply(schema._directives, [ToUpperDirective])
+applySchemaCustomDirectives(schema)
+
+export default schema
